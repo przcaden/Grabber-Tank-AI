6933ada13abfcc8a1025f5843208bad69a2fa383
Caden Perez <przcaden@gmail.com>
2022-11-25 13:42:31 -0600

    comment fix

--- robot/main.py
+++ robot/main.py
@@ -51,126 +51,126 @@ def stream_request(stream):
 
 ####################################### ADEEPT PRE-DEFINED METHODS #######################################
 
-def ap_thread():
-    os.system("sudo create_ap wlan0 eth0 Adeept 12345678")
+# def ap_thread():
+#     os.system("sudo create_ap wlan0 eth0 Adeept 12345678")
 
-async def check_permit(websocket):
-    while True:
-        recv_str = await websocket.recv()
-        cred_dict = recv_str.split(":")
-        if cred_dict[0] == "admin" and cred_dict[1] == "123456":
-            response_str = "congratulation, you have connected with server\r\nnow, you can do something else"
-            await websocket.send(response_str)
-            return True
-        else:
-            response_str = "sorry, the username or password is wrong, please submit again"
-            await websocket.send(response_str)
+# async def check_permit(websocket):
+#     while True:
+#         recv_str = await websocket.recv()
+#         cred_dict = recv_str.split(":")
+#         if cred_dict[0] == "admin" and cred_dict[1] == "123456":
+#             response_str = "congratulation, you have connected with server\r\nnow, you can do something else"
+#             await websocket.send(response_str)
+#             return True
+#         else:
+#             response_str = "sorry, the username or password is wrong, please submit again"
+#             await websocket.send(response_str)
 
-def wifi_check():
-    try:
-        s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-        s.connect(("1.1.1.1",80))
-        ipaddr_check=s.getsockname()[0]
-        s.close()
-        print(ipaddr_check)
-    except:
-        ap_threading=threading.Thread(target=ap_thread)   # Define a thread for data receiving
-        ap_threading.setDaemon(True)                          # 'True' means it is a front thread,it would close when the mainloop() closes
-        ap_threading.start()                                  # Thread starts
+# def wifi_check():
+#     try:
+#         s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+#         s.connect(("1.1.1.1",80))
+#         ipaddr_check=s.getsockname()[0]
+#         s.close()
+#         print(ipaddr_check)
+#     except:
+#         ap_threading=threading.Thread(target=ap_thread)   # Define a thread for data receiving
+#         ap_threading.setDaemon(True)                          # 'True' means it is a front thread,it would close when the mainloop() closes
+#         ap_threading.start()                                  # Thread starts
 
-def FPV_thread():
-    global fpv
-    fpv=FPV.FPV()
-    fpv.capture_thread(addr[0])
+# def FPV_thread():
+#     global fpv
+#     fpv=FPV.FPV()
+#     fpv.capture_thread(addr[0])
 
-async def recv_msg(websocket):
-    global speed_set, modeSelect
-    move.setup()
-    direction_command = 'no'
-    turn_command = 'no'
+# async def recv_msg(websocket):
+#     global speed_set, modeSelect
+#     move.setup()
+#     direction_command = 'no'
+#     turn_command = 'no'
 
-    while True: 
-        response = {
-            'status' : 'ok',
-            'title' : '',
-            'data' : None
-        }
+#     while True: 
+#         response = {
+#             'status' : 'ok',
+#             'title' : '',
+#             'data' : None
+#         }
 
-        data = ''
-        data = await websocket.recv()
-        try:
-            data = json.loads(data)
-        except Exception as e:
-            print('not A JSON')
+#         data = ''
+#         data = await websocket.recv()
+#         try:
+#             data = json.loads(data)
+#         except Exception as e:
+#             print('not A JSON')
 
-        if not data:
-            continue
+#         if not data:
+#             continue
 
-        if isinstance(data,str):
-            robotCtrl(data, response)
+#         if isinstance(data,str):
+#             robotCtrl(data, response)
 
-            switchCtrl(data, response)
+#             switchCtrl(data, response)
 
-            functionSelect(data, response)
+#             functionSelect(data, response)
 
-            configPWM(data, response)
+#             configPWM(data, response)
 
-            if 'get_info' == data:
-                response['title'] = 'get_info'
-                response['data'] = [info.get_cpu_tempfunc(), info.get_cpu_use(), info.get_ram_info()]
+#             if 'get_info' == data:
+#                 response['title'] = 'get_info'
+#                 response['data'] = [info.get_cpu_tempfunc(), info.get_cpu_use(), info.get_ram_info()]
 
-            if 'wsB' in data:
-                try:
-                    set_B=data.split()
-                    speed_set = int(set_B[1])
-                except:
-                    pass
+#             if 'wsB' in data:
+#                 try:
+#                     set_B=data.split()
+#                     speed_set = int(set_B[1])
+#                 except:
+#                     pass
 
-            elif 'AR' == data:
-                modeSelect = 'AR'
-                try:
-                    fpv.changeMode('ARM MODE ON')
-                except:
-                    pass
+#             elif 'AR' == data:
+#                 modeSelect = 'AR'
+#                 try:
+#                     fpv.changeMode('ARM MODE ON')
+#                 except:
+#                     pass
 
-            elif 'PT' == data:
-                modeSelect = 'PT'
-                try:
-                    fpv.changeMode('PT MODE ON')
-                except:
-                    pass
+#             elif 'PT' == data:
+#                 modeSelect = 'PT'
+#                 try:
+#                     fpv.changeMode('PT MODE ON')
+#                 except:
+#                     pass
 
-            #CVFL
-            elif 'CVFL' == data:
-                flask_app.modeselect('findlineCV')
+#             #CVFL
+#             elif 'CVFL' == data:
+#                 flask_app.modeselect('findlineCV')
 
-            elif 'CVFLColorSet' in data:
-                color = int(data.split()[1])
-                flask_app.camera.colorSet(color)
+#             elif 'CVFLColorSet' in data:
+#                 color = int(data.split()[1])
+#                 flask_app.camera.colorSet(color)
 
-            elif 'CVFLL1' in data:
-                pos = int(data.split()[1])
-                flask_app.camera.linePosSet_1(pos)
+#             elif 'CVFLL1' in data:
+#                 pos = int(data.split()[1])
+#                 flask_app.camera.linePosSet_1(pos)
 
-            elif 'CVFLL2' in data:
-                pos = int(data.split()[1])
-                flask_app.camera.linePosSet_2(pos)
+#             elif 'CVFLL2' in data:
+#                 pos = int(data.split()[1])
+#                 flask_app.camera.linePosSet_2(pos)
 
-            elif 'CVFLSP' in data:
-                err = int(data.split()[1])
-                flask_app.camera.errorSet(err)
+#             elif 'CVFLSP' in data:
+#                 err = int(data.split()[1])
+#                 flask_app.camera.errorSet(err)
 
-            elif 'defEC' in data:#Z
-                fpv.defaultExpCom()
+#             elif 'defEC' in data:#Z
+#                 fpv.defaultExpCom()
 
-        elif(isinstance(data,dict)):
-            if data['title'] == "findColorSet":
-                color = data['data']
-                flask_app.colorFindSet(color[0],color[1],color[2])
+#         elif(isinstance(data,dict)):
+#             if data['title'] == "findColorSet":
+#                 color = data['data']
+#                 flask_app.colorFindSet(color[0],color[1],color[2])
 
-        print(data)
-        response = json.dumps(response)
-        await websocket.send(response)
+#         print(data)
+#         response = json.dumps(response)
+#         await websocket.send(response)
 
 
 
@@ -182,7 +182,7 @@ def main():
 
     # Start preview and warm up camera for 2 seconds
     init.cam.start_preview()
-    utime.sleep(2)
+    time.sleep(2)
 
     # Construct a stream to hold image data
     stream = io.BytesIO()
@@ -207,7 +207,7 @@ def main():
         stream.truncate()
 
     # Main AI running block
-    base_time = utime.time()
+    base_time = time.time()
     while not verify:
         # Run continuous stream of video from RPi camera
         for foo in init.cam.capture_continuous(stream, 'jpeg'):
@@ -234,13 +234,13 @@ def main():
                     row, col = path.dfs(row, col, arrow, detected_objects, img)
                     
                 elif status == 'redirect_left':
-                    cur_time = utime.time()
+                    cur_time = time.time()
                     path.newTime(cur_time, base_time)
                     move.move(speed_set, 'no', 'left', 0.25)
                     approaching = True
 
                 elif status == 'redirect_right':
-                    cur_time = utime.time()
+                    cur_time = time.time()
                     base_time = path.newTime(cur_time, base_time)
                     move(speed_set, 'no', 'right', 0.25)
                     approaching = True
@@ -258,7 +258,7 @@ def main():
             # Perform decisionmaking for backtracking if the object has been picked up
             else:
                 led.colorWipe(0, 255, 0) # display green LED
-                if status == 'wall' or utime.time() - base_time <= 0.1:
+                if status == 'wall' or time.time() - base_time <= 0.1:
                     move.motorStop()
 
                     # If path has been fully backtracked (is empty) and time has elapsed, place the object back down
@@ -294,15 +294,15 @@ def main():
         else: verify = True
 
     # Display party lights
-    utime.sleep(2)
-    base_time = utime.time()
-    while utime.time() < base_time+10:
+    time.sleep(2)
+    base_time = time.time()
+    while time.time() < base_time+10:
         led.colorWipe(255, 0, 0) # display red LED
-        utime.sleep(0.5)
+        time.sleep(0.5)
         led.colorWipe(0, 0, 255) # display blue LED
-        utime.sleep(0.5)
+        time.sleep(0.5)
         led.colorWipe(0, 255, 0) # display green LED
-        utime.sleep(0.5)
+        time.sleep(0.5)
 
     # Write a length of zero to the stream to signal we're done
     connection.write(struct.pack('<L', 0))

eb69637bfd5780e3697c238eb25150d83192b2e4
Caden Perez <przcaden@gmail.com>
2022-11-30 22:17:08 -0600

    more testing

--- test_pc.py
+++ test_pc.py
@@ -1,22 +1,43 @@
-import cv2
-import zmq
-import base64
-import numpy as np
+import io
+import socket
+import struct
+from PIL import Image
+import matplotlib.pyplot as pl
 
-context = zmq.Context()
-footage_socket = context.socket(zmq.SUB)
-footage_socket.bind('tcp://*:5555')
-footage_socket.setsockopt_string(zmq.SUBSCRIBE, np.unicode(''))
+server_socket = socket.socket()
+server_socket.bind(('', 8000))  # ADD IP HERE
+server_socket.listen(0)
 
-while True:
-    try:
-        frame = footage_socket.recv_string()
-        img = base64.b64decode(frame)
-        npimg = np.fromstring(img, dtype=np.uint8)
-        source = cv2.imdecode(npimg, 1)
-        cv2.imshow("Stream", source)
-        cv2.waitKey(1)
+# Accept a single connection and make a file-like object out of it
+connection = server_socket.accept()[0].makefile('rb')
+try:
+    img = None
+    while True:
+        # Read the length of the image as a 32-bit unsigned int. If the
+        # length is zero, quit the loop
+        image_len = struct.unpack('<L', connection.read(struct.calcsize('<L')))[0]
+        if not image_len:
+            break
+        # Construct a stream to hold the image data and read the image
+        # data from the connection
+        image_stream = io.BytesIO()
+        image_stream.write(connection.read(image_len))
+        # Rewind the stream, open it as an image with PIL and do some
+        # processing on it
+        image_stream.seek(0)
+        image = Image.open(image_stream)
+        
+        if img is None:
+            img = pl.imshow(image)
+        else:
+            img.set_data(image)
 
-    except KeyboardInterrupt:
-        cv2.destroyAllWindows()
-        break
+        pl.pause(0.01)
+        pl.draw()
+
+        print('Image is %dx%d' % image.size)
+        image.verify()
+        print('Image is verified')
+finally:
+    connection.close()
+    server_socket.close()

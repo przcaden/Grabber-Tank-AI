15c35a49b9c8c003b2b8f42e7ad46c8636ac9aed
Caden Perez <przcaden@gmail.com>
2022-12-05 14:36:52 -0600

    fix functions

--- rpi_client.py
+++ rpi_client.py
@@ -93,16 +93,12 @@ class Path:
         self.turns.append(t)
 
     # Detect if an object or wall is in view and decide what action to take
-    def wallDetected(detected_objects, img):
+    def wallDetected(closest_object, img):
         dis = sense.ultra() # distance in cm
 
         if dis < 3 and dis > 1:
             move.motorStop() # stop moving
-            if len(detected_objects) > 0:
-                max_size = 0
-                for x in detected_objects:
-                    if x.size() > max_size:
-                        closest_object = x
+            if closest_object is not None:
                 x = closest_object.x
                 w = closest_object.w
 
@@ -147,11 +143,10 @@ def findObjects(img):
     contours = cv2.findContours(clean, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     contours = contours[0] if len(contours) == 2 else contours[1]
 
-    # Detect if detected red is a cube shape
-    detected_objects = []
+    # Detect if detected red object is a cube shape, and grab only the closest object
     closest_obj = None
     for c in contours:
-        epsilon = 0.05  * cv2.arcLength(c, True)
+        epsilon = 0.01 * cv2.arcLength(c, True)
         if (closest_obj is None or cv2.contourArea(c) > cv2.contourArea(closest_obj)) and len(cv2.approxPolyDP(c, epsilon, True)) == 4:
                 closest_obj = c
     
@@ -186,6 +181,9 @@ def main_logic():
             # Get a snapshot from RPi camera and 
             img = frame.array
             img, closest_obj = findObjects(img)
+            status = path.wallDetected(closest_obj, img)
+
+            print(status)
                 
             # Display edited image
             cv2.imshow('Stream', img)
@@ -199,43 +197,16 @@ def main_logic():
         # Run continuous stream of video from RPi camera
         for frame in cam.capture_continuous(rawCapture, CAM_RES, format="bgr"):
             # Get a snapshot from RPi camera and detect if any objects are in view
-            img = stream_request(stream)
-            hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-            thresh = cv2.inRange(hsv, lower, upper)
-            
-            # Apply morphology to image
-            clean = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel1)
-            clean = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel2)
-
-            # Get any external contours containg the color red
-            contours = cv2.findContours(clean, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-            contours = contours[0] if len(contours) == 2 else contours[1]
-
-            # Detect if detected red is a cube shape
-            detected_objects = []
-            for c in contours:
-                epsilon = 0.05  * cv2.arcLength(c, True)
-                if cv2.contourArea(c) > 100 and len(cv2.approxPolyDP(c, epsilon, True)) == 4:
-                    detected_objects.append(c)
-                    
-            # Write largest detected red object to image
-            if detected_objects:
-                closest_obj = None
-                for obj in detected_objects:
-                    if cv2.contourArea(obj) > cv2.contourArea(closest_obj) or closest_obj == None:
-                        closest_obj = obj
-                rot_rect = cv2.minAreaRect(closest_obj)
-                box = cv2.boxPoints(rot_rect)
-                box = numpy.int0(box)
-                cv2.drawContours(img,[box],0,(0,0,0),2)
-            
+            img = frame.array
+            img, closest_object = findObjects(img)
+                
             # Display edited image
-            cv2.imshow('frame', img)
+            cv2.imshow('Stream', img)
             cv2.waitKey(1)
             rawCapture.truncate(0)
 
             # Create a status flag depending on distance from detected objects or walls
-            status = path.wallDetected(detected_objects, img)
+            status = path.wallDetected(closest_object, img)
 
             # Generate string representation for node in path
             key = str(row) + ',' + str(col)
@@ -268,6 +239,10 @@ def main_logic():
                         grab_sequence = 0
                         base_time = path.times.pop()
 
+                    else:
+                        move(speed_set, 'forward', 'no', 0)
+
+                # If area has been travelled, turn right
                 elif key in path.visited:
                     move.motorStop()
                     move(speed_set, 'no', 'right', 0.25)
@@ -314,12 +289,7 @@ def main_logic():
             stream = io.BytesIO()
         else: verify = True
 
-    # Write a length of zero to the stream to signal algorithm is done
-    connection.write(struct.pack('<L', 0))
-
-    # Code cleanup before closing program
-    connection.close()
-    client_socket.close()
+    # Clean up GPIO and exit
     move.destroy()
 
 

e03d4351928d99d90d804e1b2fc8860263b56d23
Caden Perez <przcaden@gmail.com>
2022-12-10 18:44:36 -0600

    move testing

--- adeeptlib/move.py
+++ adeeptlib/move.py
@@ -143,9 +143,10 @@ if __name__ == '__main__':
 	try:
 		speed_set = 60
 		setup()
-		move(speed_set, 'forward', 'no', 0.8)
-		time.sleep(1.3)
-		motorStop()
+		while 1:
+			move(speed_set, 'forward', 'no', 0.8)
+			time.sleep(1.3)
+			motorStop()
 		destroy()
 	except KeyboardInterrupt:
 		destroy()
